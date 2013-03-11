610222a8f298ca7ac39f8074f541236eaa52cc7d
rsa <rsa_m@mail.ru>
2013-03-11 23:30:26 +0400

    Add usertarif field

--- sqlcon.php
+++ sqlcon.php
@@ -64,6 +64,12 @@ function sqlcon_plugin_mainloop(&$SQLCON, $start_pos, $tag_start, $tag_stop)
 		jexit();
 	}
 	
+	// Чтение username из таблицы User
+	$user = &JFactory::getUser();
+			
+	$username = $user->get('id');
+	if($username == null) $username = 0;
+			
 	// Цикл составления запросов к базе данных
 	do {
 		$stop_pos = strpos($SQLCON->Source, $tag_stop, $start_pos);
@@ -71,7 +77,7 @@ function sqlcon_plugin_mainloop(&$SQLCON, $start_pos, $tag_start, $tag_stop)
 		//echo ' stop_pos='.$stop_pos;
 		
 		if ($stop_pos===false) {
-			// Тагов больше нет
+			// Тегов больше нет
 			$start_pos = false;
 		}
 		else {
@@ -80,14 +86,64 @@ function sqlcon_plugin_mainloop(&$SQLCON, $start_pos, $tag_start, $tag_stop)
 
 			
 			$lst = array();
+			
+			// table_name.column_name.column_name_for_where=value
+			// lst[0] = table_name
+			// lst[1] = column_name or constant 'usertarif'
+			// lst[2] = column_name_for_where=value
 			$lst = explode('.', $sbs);
 			
-			$lst[2] = explode('=', $lst[2]);
+			// table_name.usertarif.column_name_for_where=value
+			// table_name.usertarifcalc.column_name_for_where=value
 			
-			$sql = "SELECT ". $lst[1] .
-					" FROM #__". $lst[0] .
-					" WHERE ". trim($lst[2][0], ' ') . " = '". trim($lst[2][1],' ') ."';";
-			
+			if($lst[2] == "usertarif"){
+				// table_name.column_name.usertarif
+				$sql = "SELECT ". $lst[1] .
+						 " FROM #__". $lst[0] .
+						 " WHERE user_id = '". $username . "';";
+			}			
+			else{
+				$lst[2] = explode('=', $lst[2]);
+				// lst[2][0] = column_name_for_where
+				// lst[2][1] = value
+				
+				switch($lst[1]){
+					case "usertarifcalc": {
+						
+						break;
+					}
+					case "usertarif": {
+						// tarif for current user with cat.
+						$sql = "SELECT cat FROM #__tsj_account WHERE user_id = '". $username . "';";
+		
+						$db->setQuery( $sql );
+						$row =& $db->loadResult();
+		
+						// Проверка на ошибки
+						if (!$result = $db->query()) {
+							echo $db->stderr();
+						//	return false;
+						}
+						if ($row == "1") $lst[1] = "tarif_1";
+						else if ($row == "2") $lst[1] = "tarif_2";
+						else /*if ($row == "0")*/ $lst[1] = "tarif";
+						
+						$sql = "SELECT ". $lst[1] .
+								" FROM #__". $lst[0] .
+								" WHERE ". trim($lst[2][0], ' ') . " = '". trim($lst[2][1],' ') ."';";
+						
+						break;
+					}
+					default:{
+						$sql = "SELECT ". $lst[1] .
+							" FROM #__". $lst[0] .
+							" WHERE ". trim($lst[2][0], ' ') . " = '". trim($lst[2][1],' ') ."';";
+						
+						break;
+					}
+				}
+			}
+
 			//echo "<br>sql=".$sql."<br>";
 			$db->setQuery( $sql );
 			$row =& $db->loadResult();
@@ -104,7 +160,7 @@ function sqlcon_plugin_mainloop(&$SQLCON, $start_pos, $tag_start, $tag_stop)
 		}
 	} while ($start_pos!==false);
 
-	print_r($sql_table_list);
+	//print_r($sql_table_list);
 	return $tag_lst;
 }
 
